c78d77bc85fa93efa89df99b6d2789347f243048
frenkell <francescocalzona82@gmail.com>
2022-09-30 14:19:41 +0200

    port forward

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,18 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=frenkell/prediction_app:latest
+echo "Docker ID and Image: $dockerpath"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run prediction-app --image=$dockerpath --port=8000 --labels app=frenkell
 
 # Step 3:
 # List kubernetes pods
+sleep 20
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward prediction-app 8000:8000
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=frenkell/prediction_app
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login &&\
+docker image tag prediction_app $dockerpath
 
 # Step 3:
-# Push image to a docker repository
+docker push $dockerpath
